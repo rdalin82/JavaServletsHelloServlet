0ab11ae05476ea7dbf8f9f0e1f34b39710358448
rdalin82 <Robert.Dalin@gmail.com>
2016-10-01 19:40:35 -0500

    initial commit, hello servlet page

---
+++ src/HelloServlets/Lesson41.java
@@ -0,0 +1,31 @@
+package HelloServlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Lesson41
+ */
+@WebServlet("/Lesson41")
+public class Lesson41 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter output = response.getWriter();	
+		output.println("<html><body><h3>Hello Servlets</h3></body></html>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
